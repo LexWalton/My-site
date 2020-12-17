2519b461bfbdaf6faf04be431f8b045d5a5816c1
LexWalton <lexusw1993@gmail.com>
2020-12-17 14:17:31 -0500

    Practice with JavaScript
    Practice with JavaScript adding a to do list to my site.

--- practice.js
+++ practice.js
@@ -1,3 +1,4 @@
+//This is for Cool feature #1
 const ourInput = document.getElementById("ourInput")
 const ourH = document.getElementById("ourH")
 
@@ -11,3 +12,24 @@ function amF () {
   }
 }
 
+
+//This is for Cool feature #1
+let ourForm = document.getElementById("ourForm")
+let ourField = document.getElementById("ourField")
+let ourList = document.getElementById("ourList")
+
+ourForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  createIteam(ourField.value)
+})
+
+function createIteam(x) {
+  let ourHTML = `<li>${x} <button onclick="deleteItem(this)">Delete</button></li>`
+  ourList.insertAdjacentHTML("beforeend", ourHTML)
+  ourField.value = ""
+  ourField.focus
+}
+
+function deleteItem(elementToDelete) {
+  elementToDelete.parentElement.remove()
+}
